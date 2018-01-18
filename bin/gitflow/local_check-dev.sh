10788383a062cb7defc79076f24b71f90cbd5ec3
Kun Ran <rankun203@gmail.com>
2018-01-18 12:08:22 +0800

    refactor: local_check-dev

--- bin/gitflow/local_check-dev.sh
+++ bin/gitflow/local_check-dev.sh
@@ -12,5 +12,19 @@ check_command() {
   fi
 }
 
+check_file() {
+  FILE=$1
+  TIP=$2
+  if [[ -e "./${FILE}" ]]; then
+    echo "CHECK FILE: ${FILE}"
+  else
+    echo "${FILE} does NOT exist, ${TIP}"
+  fi
+}
+
+# - Checking ---------------------
 check_command git 'apt install git'
 check_command postcss 'npm i -g postcss-cli'
+
+check_file .env "cp .env.sample .env && vi .env"
+check_file nginx.tmpl "curl https://raw.githubusercontent.com/jwilder/nginx-proxy/master/nginx.tmpl > /path/to/nginx.tmpl"
