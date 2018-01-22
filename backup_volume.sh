42f10494fe0ecc60f6da7eaec02614efed4d62da
Kun Ran <kun@lodestreams.com>
2018-01-22 14:16:35 +0800

    Update backup_volume.sh

--- backup_volume.sh
+++ backup_volume.sh
@@ -3,4 +3,4 @@
 
 echo "Backing up volume $1 to $2"
 
-sudo docker run -it --rm -v $1:/volume -v $2:/backups alpine tar -cjf /backups/$1.tar.bz2 -C /volume ./
+sudo docker run -it --rm -v $1:/volume -v $2:/backups alpine tar -czf /backups/$1.tar.gz -C /volume ./
