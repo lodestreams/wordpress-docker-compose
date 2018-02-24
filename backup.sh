22673840570e1e2023999f3443c66f434bf08393
Kun Ran <kun@lodestreams.com>
2018-02-24 14:32:35 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -7,6 +7,29 @@ function backup {
   ./backup_volume.sh $1 "$DEST/$NOW/"
 }
 
+
+function confirm_or_quit {
+  read -p "Are you sure? " -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+      exit 1
+  fi
+}
+
+function delete_old_backups {
+  ls -At | tail -n +$KEEP
+  echo 'is going to be deleted,' && confirm_or_quit
+  ls -At | tail -n +$KEEP | xargs rm --
+}
+
+# Delete old backups
+cd $DEST
+old_backups=`ls -At | tail -n +$KEEP | wc -l`;
+if (( old_backups > 0 )); then
+  delete_old_backups
+fi
+
 # Create archive folder
 echo rm -rf "$DEST/$NOW", waiting for 3s
 sleep 3
@@ -24,4 +47,3 @@ for i in "${volumes_arr[@]}"
 do
    backup "$i"
 done
-
